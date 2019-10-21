21c710dd6404d845fafec4c8d9c5c6446901a041
hmori696 <kikokijyo@yahoo.co.jp>
2019-10-21 22:03:15 +0900

    20191021

--- TestDB/src/Test.java
+++ TestDB/src/Test.java
@@ -1,4 +1,3 @@
-
 public class Test {
 	public static void main(String[] args) {
 		TestUserDAO dao = new TestUserDAO();
@@ -13,5 +12,11 @@ public class Test {
 
 
 		dao.updateUserNameByUserName("taro","saburo");
+
+
+		dao.insert(4,"shiro","012");
+		dao.delete("jiro");
+		dao.delete("shiro");
+		dao.insert(4, "shiro", "012");
 	}
 }
